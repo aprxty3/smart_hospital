08d0e6441f64224ed9f529dcee171c73980b6f66
aprxrt3 <ajiprxty3@gmail.com>
2021-10-29 17:27:07 +0700

    database

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "smart-hospital";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    //set PDO error mode ke exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //menyiapkan sql dan menyambungkan parameter
+} catch (PDOException $e){
+    echo "Error : ".$e->getMessage();
+}
+
+header('Content-Type: application/json');
