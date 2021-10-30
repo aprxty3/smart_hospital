619863431ada2223d4904c55ba32464b93ec0af7
aprxrt3 <ajiprxty3@gmail.com>
2021-10-30 20:03:24 +0700

    no message

--- lib/model/dokter.dart
+++ lib/model/dokter.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
-import 'package:smart-hospital/util/config.dart';
+import 'package:smart_hospital/util/config.dart';
 
 class Dokter {
   final String idDokter, nama, hp;
+
   Dokter({this.idDokter, this.nama, this.hp});
-  factory Dokter.fromJson(map<String, dynamic> json) {
+
+  factory Dokter.fromJson(Map<String, dynamic> json) {
     return Dokter(
         idDokter: json['id_dokter'], nama: json['nama'], hp: json['hp']);
   }
@@ -16,15 +16,17 @@ class Dokter {
 List<Dokter> dokterFromJson(jsonData) {
   List<Dokter> result =
       List<Dokter>.from(jsonData.map((item) => Dokter.fromJson(item)));
+
   return result;
 }
 
-//index
-Future<List<Dokter>> fetchDokter() async {
+// index
+Future<List<Dokter>> fetchDokters() async {
   String route = AppConfig.API_ENDPOINT + "dokter/index.php";
   final response = await http.get(route);
+
   if (response.statusCode == 200) {
-    var jsonResp = jsonDecode(response.body);
+    var jsonResp = json.decode(response.body);
     return dokterFromJson(jsonResp);
   } else {
     throw Exception('Failed load $route, status : ${response.statusCode}');
