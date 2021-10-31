9e47e02d65b22dac8f75e6282becd423bb180e4d
aprxrt3 <ajiprxty3@gmail.com>
2021-10-31 19:10:58 +0700

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_hospital/page/login.dart';
@@ -7,45 +5,52 @@ import 'package:smart_hospital/page/modul_pasien/index.dart' as IndexPasien;
 import 'package:smart_hospital/page/modul_pegawai/index.dart' as IndexPegawai;
 import 'package:smart_hospital/util/session.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  @OverconstrainedError
+  @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
-  @override 
-  Widget build(BuildContext context){
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.green), home: FutureBuilder(future: _loadSession(), builder: (context, snapshot){final SharedPreferences prefs = snapshot.data; Widget result; if (snapshot.connectionState == ConnectionState.waiting){
-        result = Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
-      } else if (snapshot.connectionState == ConnectionState.done){
-        if (snapshot.hasData){
-          if(prefs.getBool(IS_LOGIN) ?? false){
-            if(prefs.getString(JENIS_LOGIN) == jenisLogin.PASIEN.toString()){
-              result = IndexPasien.IndexPage();
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: FutureBuilder(
+        future: _loadSession(),
+        builder: (context, snapshot) {
+          final SharedPreferences prefs = snapshot.data;
+          Widget result;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            result = Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              if (prefs.getBool(IS_LOGIN) ?? false) {
+                if (prefs.getString(JENIS_LOGIN) ==
+                    jenisLogin.PASIEN.toString()) {
+                  result = IndexPasien.IndexPage();
+                } else {
+                  // result home pegawai
+                  result = IndexPegawai.IndexPage();
+                }
+              } else {
+                result = LoginPage();
+              }
+            } else {
+              return Container(child: Text('Error..'));
             }
-          } else {
-            //result home pegawai
-            result = IndexPegawai.IndexPage();
           }
-        } else {
-          result = LoginPage();
-        }
-      }  else {
-        return Containe(child: Text('Error...'));
-      }
-      
-      }
-      return result;
+
+          return result;
+        },
       ),
-    )
+    );
   }
-}
